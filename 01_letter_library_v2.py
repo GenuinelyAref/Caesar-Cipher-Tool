f16698886b8d5ff70da5160b51f1ff5897e7f4cc
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-15 09:54:27 +1200

    Copy code to new component (code / testing)

--- 01_letter_library_v2.py
+++ 01_letter_library_v2.py
@@ -2,3 +2,11 @@ lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
                      "t", "u", "v", "w", "x", "y", "z"]
 uppercase_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
                      "T", "U", "V", "W", "X", "Y", "Z"]
+
+indexes = [0, 25, 26]
+for i in range(0,  3):
+    try:
+        index = uppercase_letters[indexes[i]]
+    except IndexError:
+        index = "Error - no letter at this index"
+    print("Index value {}: {}".format(indexes[i], index))

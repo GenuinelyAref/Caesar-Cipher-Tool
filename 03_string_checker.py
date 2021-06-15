c25911cd8dbec358f27cc98dcf262c2fa99c0f4f
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-16 01:22:38 +1200

    String checker

--- 03_string_checker.py
+++ 03_string_checker.py
@@ -18,6 +18,7 @@ def str_checker(valid_chars):
 lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
                      "t", "u", "v", "w", "x", "y", "z"]
 # uppercase english letters
+uppercase_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
                      "T", "U", "V", "W", "X", "Y", "Z"]
 # special characters
 special_chars = [",", " ", ".", "\"", "\'", "?", "!", "(", ")", ":", ";", "-", "/"]

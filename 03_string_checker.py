45d4d779163011f7699a96b1c6531b65fdc5715c
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-16 01:22:06 +1200

    Letter library - Trial #3

---
+++ 03_string_checker.py
@@ -0,0 +1,29 @@
+# string checker function
+
+def str_checker(valid_chars):
+    condition = False
+    while not condition:
+        text = input("Enter a text here: ")
+        text = text.lower()
+        for i in range(0, len(text)):
+            try:
+                x = valid_chars[text[i]]
+                condition = True
+            except IndexError:
+                condition = False
+                break
+
+
+# lowercase english letters
+lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                     "t", "u", "v", "w", "x", "y", "z"]
+# uppercase english letters
+                     "T", "U", "V", "W", "X", "Y", "Z"]
+# special characters
+special_chars = [",", " ", ".", "\"", "\'", "?", "!", "(", ")", ":", ";", "-", "/"]
+# All valid characters
+all_chars = lowercase_letters + uppercase_letters + special_chars
+
+
+# Main routine
+str_checker(all_chars)

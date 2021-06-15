4c485ef526fb610bc98979de5ff109fbad68dbbf
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-15 22:52:35 +1200

    Letter library - Trial #1

---
+++ 02_letter_calling_function.py
@@ -0,0 +1,19 @@
+# Letter calling function
+
+
+def letter_call(letter_library, alphabet_order):
+    return letter_library[alphabet_order-1]
+
+
+# lowercase english letters
+lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                     "t", "u", "v", "w", "x", "y", "z"]
+
+# uppercase english letters
+uppercase_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+                     "T", "U", "V", "W", "X", "Y", "Z"]
+
+# main routine
+print("The 1st letter in the alphabet is \"{}\"".format(letter_call(lowercase_letters, 1)))
+print("The 5th letter in the alphabet is \"{}\" (in uppercase)".format(letter_call(uppercase_letters, 5)))
+print("The 26th letter in the alphabet is \"{}\" (in uppercase)".format(letter_call(uppercase_letters, 26)))

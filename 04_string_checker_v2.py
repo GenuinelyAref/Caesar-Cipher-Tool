67f6e98592933fb1605a46b482f0f671b5a99002
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-18 11:54:08 +1200

    String checker - Trial #1

--- 04_string_checker_v2.py
+++ 04_string_checker_v2.py
@@ -2,16 +2,27 @@
 
 def str_checker(valid_chars):
     condition = False
+    # repeat until valid input is given
     while not condition:
+        # take input
         text = input("Enter a text here: ")
+        # change input text to lowercase
         text = text.lower()
+        # check that all characters are valid/permitted
         for i in range(0, len(text)):
             try:
-                x = valid_chars[text[i]]
+                # attempt to find the character in the list of valid characters
+                valid_chars.index(text[i])
+                # then character would be valid
                 condition = True
-            except IndexError:
+            except ValueError:
+                # if character is not found in the valid characters list, then the string is not valid
                 condition = False
+                # print error message
+                print("\033[3mSorry this is an invalid input - please letters and punctuation only\033[0m\n")
                 break
+    # when all characters are valid then the whole string is valid
+    print("\033[3mValid input\033[0m\n")
 
 
 # lowercase english letters
@@ -21,7 +32,7 @@ lowercase_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 uppercase_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
                      "T", "U", "V", "W", "X", "Y", "Z"]
 # special characters
-special_chars = [",", " ", ".", "\"", "\'", "?", "!", "(", ")", ":", ";", "-", "/"]
+special_chars = [",", " ", ".", "\"", "\'", "?", "!", "(", ")", ":", ";", "-", "_", "/", "\\"]
 # All valid characters
 all_chars = lowercase_letters + uppercase_letters + special_chars
 

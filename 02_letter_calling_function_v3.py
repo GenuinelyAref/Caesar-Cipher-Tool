9ae689b0efac91cfdffc4f007838a4da1ed462a6
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-17 14:22:30 +1200

    Letter library - Trial #4

---
+++ 02_letter_calling_function_v3.py
@@ -0,0 +1,20 @@
+# Letter calling function - version 3
+
+# Letter calling function
+def letter_call(letter_library, alphabet_order, case):
+    letter = letter_library[alphabet_order % 26]
+    if case == "uppercase":
+        return letter.upper()
+    elif case == "lowercase":
+        return letter
+
+
+# lowercase english letters
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                     "t", "u", "v", "w", "x", "y", "z"]
+
+# main routine
+print("The {}th letter in the alphabet is \"{}\"".format(27, letter_call(letters, 27, "lowercase")))
+print("The {}th letter in the alphabet is \"{}\" (in uppercase)".format(0, letter_call(letters, 0, "uppercase")))
+print("The {}th letter in the alphabet is \"{}\"".format(-5, letter_call(letters, -5, "lowercase")))
+print("The {}th letter in the alphabet is \"{}\" (in uppercase)".format(74, letter_call(letters, 74, "uppercase")))

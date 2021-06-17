4a8fc24e070be4fe823398ab1b5019a228018822
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-18 11:38:04 +1200

    Add comments

--- 03_not_blank_function.py
+++ 03_not_blank_function.py
@@ -1,10 +1,15 @@
 # blank checking function
 def not_blank(prompt, error_message):
+    # take input
     user_input = input(prompt)
+    # check if input is blank / only spaces
     while user_input.strip(" ") == "":
+        # print error message
         print("{}\n".format(error_message))
+        # retake input
         user_input = input(prompt)
     return user_input
 
 
+# main routine
 age = not_blank("How old are you? Enter here: ", "You cannot leave this blank, try again")

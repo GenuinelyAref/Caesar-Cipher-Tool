917729d992bba5a8588171c8ddbf13c6f22d17e6
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-25 11:49:28 +1200

    Integrate minimal input function to base component

--- 00_base_component.py
+++ 00_base_component.py
@@ -55,6 +55,21 @@ def str_checker(var_letter_library):
     return letter_chars_location
 
 
+# Function that checks that the string given contains the minimal characters needed for the program to work correctly
+def min_input(raw_input, var_letter_library):
+    count = 26
+    for i in range(0, len(var_letter_library)):
+        try:
+            raw_input.index(var_letter_library[i])
+            count -= 1
+        except ValueError:
+            pass
+    if count == 26:
+        return False
+    else:
+        return True
+
+
 # Lists/variables
 # english letters
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",

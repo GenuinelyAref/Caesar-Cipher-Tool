be5eb5e29ea97dc3af0b5329fd6a9f010809620c
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-06-26 21:14:13 +1200

    Add code for testing

--- 07_letter_order.py
+++ 07_letter_order.py
@@ -10,4 +10,6 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
                      "t", "u", "v", "w", "x", "y", "z"]
 
 # Main routine
-print(letter_order("C", letters))
+print("The order of the letter {} in the alphabet is: {}".format("A", letter_order("A", letters)))
+print("The order of the letter {} in the alphabet is: {}".format("n", letter_order("n", letters)))
+print("The order of the letter {} in the alphabet is: {}".format("z", letter_order("z", letters)))

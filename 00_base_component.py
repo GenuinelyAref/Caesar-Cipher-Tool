7098274fffc1074a3e5478e56b371c06967362e0
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-07-06 09:23:33 +1200

    Integrate encoding component into base program

--- 00_base_component.py
+++ 00_base_component.py
@@ -77,6 +77,35 @@ def letter_order(letter, var_letter_library):
     return var_letter_library.index(letter.lower())+1
 
 
+# Function that encodes text using key
+def encode_text(var_text, var_letter_indexes, var_key, var_letter_library):
+    # convert var_text to list
+    var_text_list = list(var_text)
+    # Set variable equal to number of letters (chars to encrypt/decrypt)
+    num_of_valid_chars = len(var_letter_indexes)
+    # repeat loop as many time as there are letters in the string
+    for i in range(0, num_of_valid_chars):
+        current_char = var_text_list[var_letter_indexes[i]]
+        try:
+            letter_index = var_letter_library.index(current_char)
+            # letter is now known to be lowercase
+            # replace original character with new encrypted character
+            calling_index = (letter_index+var_key) % 26
+            new_char = var_letter_library[calling_index]
+        except ValueError:
+            # letter is now known to be uppercase
+            letter_index = var_letter_library.index(current_char.lower())
+            calling_index = (letter_index + var_key) % 26
+            new_char = var_letter_library[calling_index].upper()
+        var_text_list[var_letter_indexes[i]] = new_char
+    # convert list back into a string
+    encoded_text = ""
+    for i in range(0, len(var_text_list)):
+        encoded_text += var_text_list[i]
+    # return encoded string to user
+    return encoded_text
+
+
 # Lists/variables
 # english letters
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",

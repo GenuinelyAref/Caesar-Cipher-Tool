993c2df60458915402d20f7676b45798ccb69d70
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-07-31 20:58:32 +1200

    Reformatted function

--- 12_letter_frequency_analysis_v2.py
+++ 12_letter_frequency_analysis_v2.py
@@ -1,27 +1,30 @@
 # Tool to calculate the frequency of each letter in any string
 
-def letter_frequency(text):
-    # english letters
-    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-               "t", "u", "v", "w", "x", "y", "z"]
-
+def letter_frequency(text, var_letter_library):
     var_text = text.lower()
     letter_frequency_dict = {}
     for i in var_text:
         keys = letter_frequency_dict.keys()
         try:
-            letters.index(i)
+            var_letter_library.index(i)
             if i in keys:
                 letter_frequency_dict[i] += 1
             else:
                 letter_frequency_dict[i] = 1
         except ValueError:
             pass
-    letter_frequency_dict = sorted(letter_frequency_dict.items(), key=lambda x: x[1])
+    letter_frequency_dict = sorted(letter_frequency_dict.items(), key=lambda x: x[1], reverse=True)
     return letter_frequency_dict
 
 
-x = "The robot clicked disapprovingly, gurgled briefly inside its cubical interior and extruded a pony glass of " \
+# english letters
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+           "t", "u", "v", "w", "x", "y", "z"]
+
+
+n = "The robot clicked disapprovingly, gurgled briefly inside its cubical interior and extruded a pony glass of " \
     "brownish liquid. \"Sir, you will undoubtedly end up in a drunkard's grave, dead of hepatic cirrhosis," \
     "\" it informed me virtuously as it returned my ID card. I glared as I pushed the glass across the table. "
-print(letter_frequency(x))
+
+print(letter_frequency(n, letters))
+

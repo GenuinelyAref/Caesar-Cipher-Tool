5098be333d0296354aff3b5b5152520fdec703a9
GenuinelyAref <osmana69529@masseyhigh.school.nz>
2021-08-09 11:00:24 +1200

    Semi-final commit
    Add comments
    Make variables name more conventional
    Increase code efficieny

--- CSR_CPHR_Program.py
+++ CSR_CPHR_Program.py
@@ -5,47 +5,51 @@ import random
 
 
 # FUNCTIONS
-# random key-picking function
+# random key-picking function - for encoding without key
 def random_key_pick():
     return random.randint(1, 25)
 
 
-# integer checker for cipher key value
-def integer_check(num):
+# integer checker for cipher key value - to check that key is valid/usable
+def integer_check(var_num):
     try:
-        int(num)
+        # try converting value into integer
+        int(var_num)
+        # if successful then given value is an integer
         return True
     except ValueError:
+        # if not successful then given value cannot be an integer (string, float etc..)
         return False
 
 
-# letter calling function
-def letter_call(var_letter_library, alphabet_order, case):
+# letter calling function - to call letters by their order in the alphabet
+def letter_call(var_letter_library, var_alphabet_order, var_case):
     # find letter in english alphabet
-    letter = var_letter_library[(alphabet_order-1) % 26]
-    if case == "uppercase":
+    letter = var_letter_library[(var_alphabet_order-1) % 26]
+    if var_case == "uppercase":
         # uppercase letter
         return letter.upper()
-    elif case == "lowercase":
+    elif var_case == "lowercase":
         # lowercase letter
         return letter
 
 
-# blank checking function
-def not_blank(prompt, error_message):
+# blank checking function - check that the input is not blank
+def not_blank(var_prompt, var_error_message):
     # take input
-    user_input = input(prompt)
+    var_user_input = input(var_prompt)
     # check if input is blank / only spaces
-    while user_input.strip(" ") == "":
+    while var_user_input.strip(" ") == "":
         # print error message
-        print("{}\n".format(error_message))
+        print("{}\n".format(var_error_message))
         # retake input
-        user_input = input(prompt)
-    return user_input
+        var_user_input = input(var_prompt)
+    return var_user_input
 
 
-# string checker function
+# string checker function - returns the positions of all decryptable/encryptable characters
 def str_checker(var_text, var_letter_library):
+    # change all letters in the string to lowercase
     var_text = var_text.lower()
     letter_chars_location = []
     # check that all characters are valid/permitted
@@ -61,52 +65,69 @@ def str_checker(var_text, var_letter_library):
             pass
     # line break
     print("\n")
+    # return all the positions of the letters in the string
     return letter_chars_location
 
 
-# Function that checks that input given is either yes or no
-def yes_no_checker(prompt, proceed_affirmative, proceed_negative, error_message):
+# Function that checks that input given is either yes or no - repeats prompt if neither answer is given
+def yes_no_checker(var_prompt, var_proceed_affirmative, var_proceed_negative, var_error_message):
     valid_two = False
+    # repeat until answer given is either 'yes' or 'no'
     while not valid_two:
-        answer = input(prompt)
+        # ask question
+        answer = input(var_prompt)
+        # change all letters in the string to lowercase
         answer = answer.lower()
         if answer == "yes":
-            print(proceed_affirmative)
+            # print matching statement
+            print(var_proceed_affirmative)
             return True
         elif answer == "no":
-            print(proceed_negative)
+            # print matching statement
+            print(var_proceed_negative)
             return False
         else:
-            print(error_message)
+            # print error message + don't exit loop
+            print(var_error_message)
 
 
 # Function that checks that the string given contains the minimal characters needed for the program to work correctly
-def min_input(raw_input, var_letter_library):
+def min_input(var_raw_input, var_letter_library):
     count = 26
+    # repeat loop for each character in the string
     for i in range(0, len(var_letter_library)):
         try:
-            raw_input.index(var_letter_library[i])
+            # try to index the character in the letter library
+            var_raw_input.index(var_letter_library[i])
+            # if successful then change the count variable
             count -= 1
         except ValueError:
+            # if unsuccessful then keep the count variable the same
             pass
+    # if count is unchanged then there are no letters in the given string
     if count == 26:
         return False
+    # if count has changed then there are one or more letters in the given string
     else:
         return True
 
 
 # Function that finds letter order in alphabet and returns it
-def letter_order(letter, var_letter_library):
-    return var_letter_library.index(letter.lower())+1
+def letter_order(var_letter, var_letter_library):
+    return var_letter_library.index(var_letter.lower())+1
 
 
 # Function that calculates the likelihood of the outcome
-def likelihood_of_outcome(letter, var_standard_letters, var_standard_values):
-    var_index = var_standard_letters.index(letter)
+def likelihood_of_outcome(var_letter, var_standard_letters, var_standard_values):
+    # Find the order of the letter in the standard letter frequency table
+    var_index = var_standard_letters.index(var_letter)
     var_sum = 0
+    # Add all the frequency values up to the index of that letter
     for i in range(0, var_index + 1):
         var_sum += var_standard_values[i]
-    return round(100 - var_sum, 3)
+    # Calculate the percentage and round to 3 sf
+    percentage = round(100 - var_sum, 3)
+    return percentage
 
 
 # Letter frequency analysis tool
@@ -160,6 +181,7 @@ def encode_text(var_text, var_letter_indexes, var_key, var_letter_library):
 
 # Function to decode text using key
 def decode_text(var_text, var_letter_indexes, var_key, var_letter_library):
+    decoded_text = ""
     # convert var_text to list
     var_text_list = list(var_text)
     # Set variable equal to number of letters (chars to decrypt/decrypt)
@@ -180,7 +202,6 @@ def decode_text(var_text, var_letter_indexes, var_key, var_letter_library):
             new_char = var_letter_library[calling_index].upper()
         var_text_list[var_letter_indexes[i]] = new_char
     # convert list back into a string
-    decoded_text = ""
     for i in range(0, len(var_text_list)):
         decoded_text += var_text_list[i]
     # return decoded string to user
@@ -194,13 +215,17 @@ def decode_without_key(var_text, var_letter_indexes, var_letter_library, var_mos
     var_key = 0
     # Set variable equal to number of letters (chars to decrypt/decrypt)
     num_of_valid_chars = len(var_letter_indexes)
-    # decoding without key part:
     correct = False
     letter_count = 0
+    # find index of the most frequent letter
+    most_frequent_letter_index = var_letter_library.index(var_most_frequent_letter)
+    # repeat until user claims that the string is decoded correctly
     while not correct:
-        a = var_letter_library.index(var_most_frequent_letter)
-        b = var_letter_library.index(var_standard_freq_letters[letter_count])
-        var_key = (26 - (b - a)) % 26
+        # find index of the next-most frequent letter's index in the standard letter frequency table
+        next_most_standard_frequent_letter_index = var_letter_library.index(var_standard_freq_letters[letter_count])
+        # set the key equal to the difference between both indexes
+        var_key = (26 - (next_most_standard_frequent_letter_index - most_frequent_letter_index)) % 26
+        # to increase the progression of standard letter table values
         letter_count += 1
         # convert var_text to list
         var_text_list = list(var_text)
@@ -220,13 +245,14 @@ def decode_without_key(var_text, var_letter_indexes, var_letter_library, var_mos
                 new_char = var_letter_library[calling_index].upper()
             var_text_list[var_letter_indexes[i]] = new_char
         # convert list back into a string
-        decoded_text = ""
         for i in range(0, len(var_text_list)):
             decoded_text += var_text_list[i]
         correct_key = yes_no_checker("Is this the correct text?\033[1m\n{}\033[0m\n\nYes/no: ".format(decoded_text),
-                                     "Success!", "\n\033[3mtrying again\033[0m\n", "Please enter yes/no")
+                                     "Success!", "\033[3m\ntrying again\033[0m\n", "Please enter yes/no")
+        # if decoded text is claimed correct by the user, break loop
         if correct_key:
             break
+        # if decoded text is incorrect, continue loop
         else:
             continue
     # return decoded string to user
@@ -234,7 +260,7 @@ def decode_without_key(var_text, var_letter_indexes, var_letter_library, var_mos
 
 
 # Program instructions
-def instructions(condition_met):
+def instructions(var_condition_met):
     instructions_text = " | To use this tool, you will need to have a very basic understanding of Caesar Ciphers.\n" \
                    " | If you are unfamiliar with the concept, I highly recommend visiting this website\n" \
                    " | (https://csfieldguide.org.nz/en/chapters/coding-encryption/substitution-ciphers/)\n" \
@@ -244,7 +270,7 @@ def instructions(condition_met):
                    " | any text without a given key in the least theoretical possible number of steps. You\n" \
                    " | will be required to enter the following information:\n\n | 1) text (can contain numbers, " \
                    "punctuation etc...)\n | 2) a key (if you have one)\n | 3) whether you want to encode or decode"
-    if not condition_met:
+    if not var_condition_met:
         print(instructions_text)
     else:
         print("\033[3mSkipping instructions >>\033[0m")
@@ -260,7 +286,7 @@ standard_freq_letters = ["e", "t", "a", "o", "i", "n", "s", "h", "r", "d", "l", 
 # Standard letter frequency table (values)
 standard_freq_values = [12.702, 9.056, 8.167, 7.507, 6.966, 6.749, 6.327, 6.094, 5.987, 4.253, 4.025, 2.782, 2.758,
                         2.406, 2.36, 2.228, 2.015, 1.974, 1.929, 1.492, 0.978, 0.772, 0.153, 0.15, 0.095, 0.074]
-user_input= ""
+user_input = ""
 key = 0
 
 # MAIN ROUTINE
@@ -304,16 +330,17 @@ if user_has_key:
         print("\n\033[3mMake sure your key is an integer\033[0m")
         key = input("Enter your key here: ")
         valid_key = integer_check(key)
-    key=int(key)
+    key = int(key)
 
 
 if text_function == "decode":
     if user_has_key:
         result = decode_text(user_input, str_checker(user_input, letters), key, letters)
     else:
-        result = decode_without_key(user_input, str_checker(user_input, letters), letters, letter_frequency(user_input, letters), standard_freq_letters)
-    print("\n\033[1mKey: \033[0m\033[3m{}\033[0m\n\n\033[0m\033[1mOriginal text:\033[0m\n\033[3m{}\033[0m\n\n\033["
-          "1mDecoded text:\033[0m\n\033[3m{}\033[0m".format(result[1], user_input, result[0]))
+        result = decode_without_key(user_input, str_checker(user_input, letters), letters,
+                                    letter_frequency(user_input, letters), standard_freq_letters)
+    print("\n\033[1m | Key: \033[0m\033[3m{}\033[0m\n\n | \033[1mOriginal text:\033[0m\n | \033[3m"
+          "{}\033[0m\n\n | \033[1mDecoded text:\033[0m\n | \033[3m{}\033[0m".format(result[1], user_input, result[0]))
 else:
     if user_has_key:
         result = encode_text(user_input, str_checker(user_input, letters), key, letters)
@@ -321,5 +348,5 @@ else:
         key = random_key_pick()
         print("\033[3mRandom key generated\033[0m")
         result = encode_text(user_input, str_checker(user_input, letters), key, letters)
-    print("\n\033[1m | Key: \033[0m\033[3m{}\033[0m\n\n\033[0m\033[1m | Original text:\033[0m\n\033[3m{}\033[0m\n\n\033["
-          "1m | Encoded text:\033[0m\n\033[3m{}\033[0m".format(result[1], user_input, result[0]))
+    print("\n\033[1m | Key: \033[0m\033[3m{}\033[0m\n\n | \033[1mOriginal text:\033[0m\n | \033[3m"
+          "{}\033[0m\n\n | \033[1mEncoded text:\033[0m\n | \033[3m{}\033[0m".format(result[1], user_input, result[0]))
